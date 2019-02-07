17cc89ae484bac4f7de891f9a4daa029f18c2e30
Ishita Korde <ikord001@ucr.edu>
2019-02-07 10:33:59 -0800

    lab6,8

---
+++ ikord001_lab8/ikord001_lab8_part1/ikord001_lab8_part1/ikord001_lab8_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * ikord001_lab8_part1.c
+ *
+ * Created: 2/7/2019 10:32:02 AM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
